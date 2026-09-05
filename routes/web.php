eb1eeb1dcf212b8c3bfd280364530d8b636c29b0
Thiago <engs-thiagoluz@camporeal.edu.br>
2026-09-05 09:37:55 -0300

    ATV 1: Rotas simples

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return 'Grandioso sobre.';
+});
+
+Route::get('/alunos', function () {
+    return 'Alunos endividados.';
+});
+
+Route::get('/contato', function () {
+    return 'Me liga.';
+});
