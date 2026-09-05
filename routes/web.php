edfcdf7bced91e2339ac9ddf14b5215e7fdcd3d4
Thiago <engs-thiagoluz@camporeal.edu.br>
2026-09-05 11:41:53 -0300

    ATV 18: Autenticação com Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,20 @@
 <?php
 
-
-use App\Models\Aluno;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AlunoController;
 
-Route::get('/sobre', function () {
-    return 'Grandioso sobre.';
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/alunos', function () {
-    return 'Alunos endividados.';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/contato', function () {
-    return 'Me liga.';
-});
-
-Route::get('/produto/{id}', function ($id) {
-    return "Produto com ID: {$id}";
-});
-
-Route::get('/categoria/{id}', function ($id) {
-    return "Categoria com ID: {$id}";
-});
-
-Route::get('/usuario/{id}', function ($id) {
-    return "Usuário com ID: {$id}";
-});
-
-Route::resource('alunos', AlunoController::class);
-
-Route::get('/alunos/curso/{curso}', function ($curso) {
-    return Aluno::doCurso($curso)->get();
-});
-
-Route::get('/alunos/busca/{palavra}', function ($palavra) {
-    return Aluno::nomeContem($palavra)->get();
-});
-
-Route::get('/alunos/recentes', function () {
-    return Aluno::recentes()->get();
-});
-
-Route::get('/alunos/quantidade', function () {
-    return Aluno::count();
-});
+require __DIR__.'/auth.php';
