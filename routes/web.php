f740fec2c51e96a77fa4444793af130b8000f112
Thiago <engs-thiagoluz@camporeal.edu.br>
2026-09-05 09:50:31 -0300

    ATV 2: Rota com parâmetro

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/alunos', function () {
 Route::get('/contato', function () {
     return 'Me liga.';
 });
+
+Route::get('/produto/{id}', function ($id) {
+    return "Produto com ID: {$id}";
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Categoria com ID: {$id}";
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Usuário com ID: {$id}";
+});
