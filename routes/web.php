e9553f88b0bf632b8975f6e7f5cc3e5e5bfa3ae1
Thiago <engs-thiagoluz@camporeal.edu.br>
2026-09-05 11:52:00 -0300

    ATV 21: Middleware para admin e professor

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin', function () {
+        return 'Área do adm.';
+    });
+});
+
+Route::middleware(['auth', 'role:admin,professor'])->group(function () {
+    Route::get('/professor', function () {
+        return 'Área do professor.';
+    });
 });
 
 require __DIR__.'/auth.php';
