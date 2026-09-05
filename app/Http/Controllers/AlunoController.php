d248b3aa534cec808aa7880a6e612beb521d3d9c
Thiago <engs-thiagoluz@camporeal.edu.br>
2026-09-05 10:05:38 -0300

    ATV 4: 7 CRUDS

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    private static array $alunos = [
+        1 => ['id' => 1, 'nome' => 'João Silva', 'email' => 'joao@email.com'],
+        2 => ['id' => 2, 'nome' => 'Maria Souza', 'email' => 'maria@email.com'],
+    ];
+
+    public function index()
+    {
+        return self::$alunos;
+    }
+    public function show($id)
+    {
+        return self::$alunos[$id] ?? "Aluno não encontrado.";
+    }
+
+    public function create()
+    {
+        return "Formulário de criação de aluno.";
+    }
+
+    public function store(Request $request)
+    {
+        return "Aluno cadastrado!";
+    }
+
+    public function edit($id)
+    {
+        return "Formulário de edição do aluno {$id}.";
+    }
+
+    public function update(Request $request, $id)
+    {
+        return "Aluno {$id} atualizado";
+    }
+
+    public function destroy($id)
+    {
+        return "Aluno {$id} removido ";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,3 +30,11 @@ Route::get('/categoria/{id}', function ($id) {
 Route::get('/usuario/{id}', function ($id) {
     return "Usuário com ID: {$id}";
 });
+
+Route::get('/alunos-crud', [AlunoController::class, 'index']);
+Route::get('/alunos-crud/create', [AlunoController::class, 'create']);
+Route::post('/alunos-crud', [AlunoController::class, 'store']);
+Route::get('/alunos-crud/{id}', [AlunoController::class, 'show']);
+Route::get('/alunos-crud/{id}/edit', [AlunoController::class, 'edit']);
+Route::put('/alunos-crud/{id}', [AlunoController::class, 'update']);
+Route::delete('/alunos-crud/{id}', [AlunoController::class, 'destroy']);
